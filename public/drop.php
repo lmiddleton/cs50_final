93c01d580fea7ac6c6130c837054212ae48a40b2
lmiddleton <lkm@LKM-MacBook.local>
2013-12-02 19:33:07 -0500

    temp

--- public/drop.php
+++ public/drop.php
@@ -37,8 +37,6 @@
       		}
     		else
       		{
-      			// resize it and store
-      		
       			move_uploaded_file($_FILES["file"]["tmp_name"],
       			"upload/" . $_FILES["file"]["name"]);
       			//echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
